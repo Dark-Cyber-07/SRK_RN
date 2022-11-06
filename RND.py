6cb5ad7e308b787c985dded992e41083fe77eb13
Mr. Loser <85592998+Dark-Cyber-07@users.noreply.github.com>
2022-11-06 12:42:19 +0600

    Add files via upload

---
+++ RND.py
@@ -0,0 +1,31 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from SRK64_RN import main
+ 
+        main()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from SRK32_RN import main
+ 
+ 
+        main()
+ 
